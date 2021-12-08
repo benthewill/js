d05c0e2f1a48be360888b336672007c99eecf55a
benthewill <benthewill@gmail.com>
2021-12-07 20:31:35 -0800

    learned apply

--- Practice/Jinfo/apply.js
+++ Practice/Jinfo/apply.js
@@ -1,14 +1,15 @@
-let obj = {
-    goku : `kamehameha`,
-    vegeta : `final flash`
+let goku = {
+    name : `Son Goku`,
+    attack : `Kamehameha`,
+    form : `Super Saiyan`,
 }
 
+function attack (power, intent) {
+    return `${this.name} attacked using ${power}-powered ${this.attack} with ${intent} intention`
+}
 
-let arr2 = [3,4,5,6]
+let gokuAttacks = attack.apply(goku, [`high`,`evil`])
+console.log(gokuAttacks);
 
-
-let arr3 = [1,2]
-
-arr3.push.apply(arr3, arr2)
-
-console.log(arr3);
+let gokuAttacksCall = attack.call(goku, `high`,`evil`)
+console.log(gokuAttacksCall);
