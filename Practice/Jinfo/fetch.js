a14cd8796c0497f94266fe6993c824ad8f6d187a
benthewill <benthewill@gmail.com>
2022-07-03 21:17:29 -0700

    Jul3_3

---
+++ Practice/Jinfo/fetch.js
@@ -0,0 +1,42 @@
+//XML HTTP Request
+
+// const { default: fetch } = require("node-fetch");
+
+import fetch from 'node-fetch'
+
+function success() {
+    let data = JSON.parse(this.responseText)
+    console.log(data);
+    let title = data.title
+    console.log(title);
+    userWork = title
+}
+
+function error() {
+    console.log('Request Failed', err);
+}
+
+let xhr = new XMLHttpRequest()
+xhr.onload = success
+xhr.onerror = error
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');
+xhr.send()
+
+let userWork;
+
+//FETCH!!!!!!!!!
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.title);
+        userWork = data.title
+    })
+
+console.log(userWork);
+
+functio
+
+
+
